88804c7a050cd3a5800e87256e5da50d2fbebcce
Flávia Domingues <flavia.domingues@matera.com>
2018-06-18 09:30:31 -0300

    Ajustando layout

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,51 +1,64 @@
-import { Component } from '@angular/core';
-import { NavController, ToastController, Toast, NavParams } from 'ionic-angular';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Component } from "@angular/core";
+import {
+  NavController,
+  ToastController,
+  Toast,
+  NavParams
+} from "ionic-angular";
+import { AngularFireDatabase } from "angularfire2/database";
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+  selector: "page-list",
+  templateUrl: "list.html"
 })
 export class ListPage {
   public matches;
-  public parametroListagem;
+  public paramTab;
+  public paramTitulo;
   private toastInstance: Toast;
 
-  constructor(public navCtrl: NavController, 
+  constructor(
+    public navCtrl: NavController,
     public navParam: NavParams,
-    public afDB: AngularFireDatabase, 
-    private toastCtrl: ToastController) {
-    this.parametroListagem = this.navParam.data.param;
-    this.get1Rodada(this.parametroListagem).subscribe((data) => {
-      let selectedDate;
-      const groupedObj = data.reduce((prev, cur)=> {
-        selectedDate = cur['data_hora'].split('T')[0];
-        if(!prev[selectedDate]) {
-          prev[selectedDate] = [cur];
-        } else {
-          prev[selectedDate].push(cur);
-        }
-        return prev;
-      }, {});
-      this.matches = Object.keys(groupedObj).map(key => ({ key, value: groupedObj[key] }));
-      console.log(this.matches);
-    }, error => {
-      console.log("error ", error);
-      this.showToastr(error);
-    });
+    public afDB: AngularFireDatabase,
+    private toastCtrl: ToastController
+  ) {
+    this.paramTab = this.navParam.data.tab;
+    this.paramTitulo = this.navParam.data.titulo;
+    this.get1Rodada(this.paramTab).subscribe(
+      data => {
+        let selectedDate;
+        const groupedObj = data.reduce((prev, cur) => {
+          selectedDate = cur["data_hora"].split("T")[0];
+          if (!prev[selectedDate]) {
+            prev[selectedDate] = [cur];
+          } else {
+            prev[selectedDate].push(cur);
+          }
+          return prev;
+        }, {});
+        this.matches = Object.keys(groupedObj).map(key => ({
+          key,
+          value: groupedObj[key]
+        }));
+      },
+      error => {
+        this.showToastr(error);
+      }
+    );
   }
 
-  get1Rodada(param) {
-    return this.afDB.list(param + "_RODADA").valueChanges();
+  get1Rodada(tab) {
+    return this.afDB.list(tab).valueChanges();
   }
 
   showToastr(message) {
     this.toastInstance = this.toastCtrl.create({
       message: message,
-      position: 'bottom',
+      position: "bottom",
       cssClass: "error",
       showCloseButton: true,
-      closeButtonText: 'Ok'
+      closeButtonText: "Ok"
     });
     this.toastInstance.present();
   }
